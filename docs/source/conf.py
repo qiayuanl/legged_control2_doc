c6a60d9072094ea5726c8687007448cfaf4ebfca
qiayuanl <qiayuanl@berkeley.edu>
2025-01-24 20:59:41 -0800

    Update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'legged_control2'
+copyright = '2025, Qiayuan Liao'
+author = 'Qiayuan Liao'
 
 release = '0.1'
 version = '0.1.0'
